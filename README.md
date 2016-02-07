8db198a87a57f174c5d661dba06b87ce9fea959c
Vikash <vikash@programminggeek.in>
2016-02-07 20:51:01 +0530

    Setup the basic App :pencil:

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # etest-beta
+This is the beta version (backend api) of etest in NodeJS.
+The current live version is at http://etest.programminggeek.in/
+#Running the example
+In order to run the example you need to have npm and nodejs installed.
+
+Run `npm install` to ensure local dependencies are available.
+
+You also need to set the ClientSecret and ClientId for your Auth0 app as enviroment variables with the following names respectively: `AUTH0_CLIENT_SECRET` and `AUTH0_CLIENT_ID`.
+
+For that, the following should have been already created for you; if not, just create a file named `.env` in the directory and set the values like the following, the app will just work:
+
+````bash
+# .env file
+AUTH0_CLIENT_SECRET=myCoolSecret
+AUTH0_CLIENT_ID=myCoolClientId
+````
+
+Once you've set those 2 enviroment variables, just run `node server.js` and try calling [http://localhost:3001/ping](http://localhost:3001/ping)
---
+++ app.js
@@ -0,0 +1,81 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var jwt = require('express-jwt');
+var dotenv = require('dotenv');
+var cors = require('cors');
+
+var routes = require('./routes/index');
+var users = require('./routes/users');
+
+dotenv.load();
+
+var authenticate = jwt({
+  secret: new Buffer(process.env.AUTH0_CLIENT_SECRET || 'qu6eQT-djPW_RsvT9z1fn_sfnr3UDCefb2kyd1MbYWejxoJpJWIlXA4S1_UAAIno', 'base64'),
+  audience: process.env.AUTH0_CLIENT_ID || 'S6MNYpDy9EEmj0DO6jch3RQfFOtEvzOI'
+});
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// Request body parsing middleware should be above methodOverride
+app.use('/secured', authenticate);//Make certain routes authenticated
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+app.use('/users', users);
+
+app.get('/ping', function(req, res) {
+  res.send(200, {text: "All good. You don't need to be authenticated to call this"});
+});
+
+app.get('/secured/ping', function(req, res) {
+  res.send(200, {text: "All good. You only get this message if you're authenticated"});
+})
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+
+module.exports = app;
