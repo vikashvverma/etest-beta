a0243df5b55c9f0951e64b5ea2dc3c7cfd615982
Vikash <vikashkv@thoughtworks.com>
2016-05-01 10:14:47 +0530

    Watch for theme change

--- client/components/chart/etest-chart-directive.js
+++ client/components/chart/etest-chart-directive.js
@@ -1,28 +1,29 @@
-angular.module("etestApp").directive('etestChart', function (ChartService,$window) {
+angular.module("etestApp").directive('etestChart', function (ChartService, $window) {
   return {
     restrict: 'EA',
     scope: {
       data: '=',
       type: '=',
-      title:"@"
+      title: "@",
+      theme: '='
     },
     link: function (scope, element, attrs) {
       scope.$watch('type', function (newType, oldType) {
-        if(!scope.data) return;
-        var categories,data=[{}];
-        if(scope.data[0].data && scope.data[0].data.categories){
-          categories=scope.data[0].data.categories;
-          data[0].data=scope.data[0].data.data;
-          data[0].name=scope.data[0].name;
+        if (!scope.data) return;
+        var categories, data = [{}];
+        if (scope.data[0].data && scope.data[0].data.categories) {
+          categories = scope.data[0].data.categories;
+          data[0].data = scope.data[0].data.data;
+          data[0].name = scope.data[0].name;
         }
         if (newType == 'pie') {
           //$(element).css({width:'98%',height:'auto',margin:'0 auto'});
           $(element).highcharts(ChartService.getPie(scope.data));
         } else {
-          $(element).highcharts(ChartService.getChart(scope.title, scope.type,categories?data:scope.data,categories?categories:undefined));
+          $(element).highcharts(ChartService.getChart(scope.title, scope.type, categories ? data : scope.data, categories ? categories : undefined, scope.theme));
         }
 
-        $(element).find('text') .last() .html('Programming Geek') .click(function (event) {
+        $(element).find('text').last().html('Programming Geek').click(function (event) {
           event.preventDefault();
           $window.location = 'http://www.programminggeek.in';
         });
@@ -30,23 +31,43 @@ angular.module("etestApp").directive('etestChart', function (ChartService,$windo
           scrollTop: $(element).offset().top
         }, 2000);
       });
-      scope.$watch('data', function (data,old) {
+      scope.$watch('data', function (data, old) {
 
-        if(!data) return;
-        var categories,rankData=[{}];
-        if(data[0].data && data[0].data.categories){
-          categories=data[0].data.categories;
-          rankData[0].data=data[0].data.data;
-          rankData[0].name=data[0].name;
+        if (!data) return;
+        var categories, rankData = [{}];
+        if (data[0].data && data[0].data.categories) {
+          categories = data[0].data.categories;
+          rankData[0].data = data[0].data.data;
+          rankData[0].name = data[0].name;
         }
         if (scope.type == 'pie') {
           $(element).highcharts(ChartService.getPie(data));
         } else {
-          $(element).css({width:'98%',height:'auto',margin:'0 auto'});
-          $(element).highcharts(ChartService.getChart(scope.title, scope.type, categories?rankData:data,categories));
+          $(element).css({width: '98%', height: 'auto', margin: '0 auto'});
+          $(element).highcharts(ChartService.getChart(scope.title, scope.type, categories ? rankData : data, categories, scope.theme));
         }
 
-        $(element).find('text') .last() .html('Programming Geek') .click(function (event) {
+        $(element).find('text').last().html('Programming Geek').click(function (event) {
+          event.preventDefault();
+          $window.location = 'http://www.programminggeek.in';
+        });
+      });
+      scope.$watch('theme', function (newTheme, oldTheme) {
+        if (!scope.data) return;
+        var categories, data = [{}];
+        if (scope.data[0].data && scope.data[0].data.categories) {
+          categories = scope.data[0].data.categories;
+          data[0].data = scope.data[0].data.data;
+          data[0].name = scope.data[0].name;
+        }
+        if (scope.type == 'pie') {
+          //$(element).css({width:'98%',height:'auto',margin:'0 auto'});
+          $(element).highcharts(ChartService.getPie(scope.data));
+        } else {
+          $(element).highcharts(ChartService.getChart(scope.title, scope.type, categories ? data : scope.data, categories ? categories : undefined, scope.theme));
+        }
+
+        $(element).find('text').last().html('Programming Geek').click(function (event) {
           event.preventDefault();
           $window.location = 'http://www.programminggeek.in';
         });
